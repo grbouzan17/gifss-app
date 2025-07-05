3c4b2d6b06ea19b3c43347cb4236617600f8b6e2
Gabriel <grbouzan@gmail.com>
2025-07-05 18:05:54 +0200

    cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   companyName2: 'App',
   companySlogan: 'Maneja tus gifs',
   //Apikeys
-  // apikey: 'necesitas una clave',
+  apikey: 'YzOgESrm1eU4UPY7fAVzQa6vv9ev6AnZ',
   giphyUrl: 'https://api.giphy.com/v1'
 };
