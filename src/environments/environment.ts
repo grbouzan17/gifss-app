a292604491c3d07af979e85adf12a7fe47fbb610
grbouzan17 <82260481+grbouzan17@users.noreply.github.com>
2025-07-05 16:52:26 +0200

    necesitas una clave

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   companyName2: 'App',
   companySlogan: 'Maneja tus gifs',
   //Apikeys
-  apikey: 'YzOgESrm1eU4UPY7fAVzQa6vv9ev6AnZ',
+  // apikey: 'YzOgESrm1eU4UPY7fAVzQa6vv9ev6AnZ',
   giphyUrl: 'https://api.giphy.com/v1'
 };
