aca667f40ebcc267e97e05720fc72ac806ba4716
Gabriel <grbouzan@gmail.com>
2025-07-05 18:12:27 +0200

    Dejar de rastrear archivos de entorno (environment.ts y environment.development.ts)

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  companyName: 'Gifs',
-  companyName2: 'App',
-  companySlogan: 'Maneja tus gifs',
-  //Apikeys
-  apikey: 'YzOgESrm1eU4UPY7fAVzQa6vv9ev6AnZ',
-  giphyUrl: 'https://api.giphy.com/v1'
-};
